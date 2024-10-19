38974d140d86d3246a0c6d9d636642f130c0b932
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-19 10:26:16 -0700

    Deploy static site to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -5,19 +5,7 @@ module.exports = {
   basePath: isProd ? '/developer-portfolio' : '',
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Needed for GitHub Pages
   },
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.(eot|ttf|otf|woff|woff2)$/,
-        type: 'asset/resource', // Use webpack's asset handling for fonts
-      },
-      {
-        test: /\.txt$/,
-        type: 'asset/source', // Treat text files as raw sources
-      }
-    );
-    return config;
-  },
+  output: 'export', // Enables static export
 };
