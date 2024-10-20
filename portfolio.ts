c0d48b5765823c6d93e9ddae46f7fb69512333dd
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-19 17:54:19 -0700

    added skills

--- portfolio.ts
+++ portfolio.ts
@@ -49,26 +49,42 @@ export const skillsSection: SkillsSectionType = {
         emoji("🌐 Large-Scale Distributed Systems, HPC Expertisem, & Cloud Proficiency"),
       ],
       softwareSkills: [
-        { skillName: "C/C++", iconifyTag: "logos:cplusplus" },
-        { skillName: "Python", iconifyTag: "logos:python" },
-        { skillName: "MPI", iconifyTag: "simple-icons:mpich" },
-        { skillName: "OpenMP", iconifyTag: "logos:openmp" },
-        { skillName: "Fortran", iconifyTag: "simple-icons:fortran" },
-        { skillName: "Java", iconifyTag: "logos:java" },
-        { skillName: "JavaScript", iconifyTag: "logos:javascript" },
-        { skillName: "PyTorch", iconifyTag: "logos:pytorch-icon" },
-        { skillName: "MuJoCo", iconifyTag: "simple-icons:mujoco" },
-        { skillName: "NVIDIA Modulus", iconifyTag: "simple-icons:nvidia" },
-        { skillName: "NVIDIA Warp", iconifyTag: "simple-icons:nvidia" },
-        { skillName: "Git", iconifyTag: "logos:git" },
-        { skillName: "CI/CD", iconifyTag: "vscode-icons:file-type-light-ci" },
-        { skillName: "Docker", iconifyTag: "logos:docker-icon" },
-        { skillName: "Google Cloud", iconifyTag: "logos:google-cloud" },
-        { skillName: "AWS", iconifyTag: "logos:aws" },
-        { skillName: "Django", iconifyTag: "vscode-icons:file-type-django" },
-        { skillName: "Reactjs", iconifyTag: "vscode-icons:file-type-reactjs" },
-        { skillName: "Nextjs", iconifyTag: "vscode-icons:file-type-light-next" },
-        { skillName: "Github", iconifyTag: "akar-icons:github-fill" }
+        {
+          skillName: "Python",
+          iconifyTag: "logos:python",
+        },
+        {
+          skillName: "Django",
+          iconifyTag: "vscode-icons:file-type-django",
+        },
+        {
+          skillName: "JavaScript",
+          iconifyTag: "logos:javascript",
+        },
+        {
+          skillName: "Reactjs",
+          iconifyTag: "vscode-icons:file-type-reactjs",
+        },
+        {
+          skillName: "Nextjs",
+          iconifyTag: "vscode-icons:file-type-light-next",
+        },
+        {
+          skillName: "AWS",
+          iconifyTag: "logos:aws",
+        },
+        {
+          skillName: "Heroku",
+          iconifyTag: "logos:heroku-icon",
+        },
+        {
+          skillName: "Github",
+          iconifyTag: "akar-icons:github-fill",
+        },
+        {
+          skillName: "Docker",
+          iconifyTag: "logos:docker-icon",
+        },
       ],
     },
   ],
