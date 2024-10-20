d81cb1ef94e92c1d5d5fbdb187b3f5f6b15c851d
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-19 18:15:03 -0700

    modified description greetings

--- portfolio.ts
+++ portfolio.ts
@@ -13,10 +13,10 @@ import {
 
 export const greetings: GreetingsType = {
   name: "Jeremie Meurisse",
-  title: "Hi all, I'm Jeremie",
+  title: "Hi, I'm Jeremie!",
   description:
-    "I am a highly skilled software engineer with over 9 years of experience at NASA Ames Research Center, I specialize in developing and leading complex multi-physics simulation projects. My journey from Intern to Lead Software Engineer has equipped me with a robust technical background and strong leadership capabilities, including 7 years of management experience.",
-  resumeLink: "#",
+    "I am a software engineer with over 9 years of experience at NASA, I specialize in developing and leading complex multi-physics simulation projects. My journey from Intern to Lead Software Engineer has equipped me with a robust technical background and strong leadership capabilities, including 7 years of management experience.",
+  resumeLink: "/2024-08-08_Jeremie_Meurisse_Resume.pdf",
 };
 
 export const openSource = {
