03f2d7554f3e2d97846b47347c3c4ebd921fe213
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-28 19:01:49 -0700

    updated core skills

--- portfolio.ts
+++ portfolio.ts
@@ -110,8 +110,8 @@ agreements with research institutes.",
   {
     img: "/img/icons/deep-learning.png",
     name: "Machine Learning",
-    desc: "3 years of ML experience. Supervised development of deep learning segmentation tools for video and 3D imaging \
-data. Trained physics-informed neural networks to create surrogate models for statistical analysis. Fine-\
+    desc: "3 years of ML integration. Supervised development of deep learning segmentation tools for video and 3D imaging \
+data. Trained PINNs to create surrogate models for statistical analysis. Fine-\
 tuned generative AI model GPT-NeoX (1.3B) to enhance knowledge management systems. Participated in ML trainings and \
 industry conferences. Facilitated collaborative sessions with external experts, enhancing team proficiency in ML."
    },
