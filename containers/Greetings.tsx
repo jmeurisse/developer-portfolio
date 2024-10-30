92ba69be6f7c5df5aa13ce8b7b4c708d954583af
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-30 12:28:54 -0700

    no separator in Greetings

--- containers/Greetings.tsx
+++ containers/Greetings.tsx
@@ -53,19 +53,6 @@ const Greetings = () => {
               </Row>
             </div>
           </Container>
-          {/* SVG separator */}
-          <div className="separator separator-bottom separator-skew">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
-            </svg>
-          </div>
         </section>
         {/* 1st Hero Variation */}
       </div>
