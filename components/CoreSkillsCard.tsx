8720c40cbba1d121804deac46345ac9bd6b18fbe
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-30 14:50:51 -0700

    justified text CoreSkills + image size

--- components/CoreSkillsCard.tsx
+++ components/CoreSkillsCard.tsx
@@ -15,7 +15,7 @@ const CoreSkillsCard = ({ img, name, desc }: CoreSkillsType) => {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              height: "8vh", 
+              height: "9vh", 
               padding: "0rem", // Ensure spacing within the container
               overflow: "hidden", // Prevent image overflow
             }}
