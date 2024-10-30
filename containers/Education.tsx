472a5948c3103ba0b93ad84d19576080bb6922f1
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-29 21:42:13 -0700

    rm bg Education

--- containers/Education.tsx
+++ containers/Education.tsx
@@ -8,7 +8,7 @@ const Education = () => {
   return (
     educationInfo && (
       <Fade bottom duration={2000}>
-        <section className="section pb-0 bg-gradient-info my-5">
+        <section className="section section-lg">
           <Container>
             <div className="d-flex px-3">
               <div>
@@ -17,7 +17,7 @@ const Education = () => {
                 </div>
               </div>
               <div className="pl-4">
-                <h4 className="display-3 text-white">Education</h4>
+                <h4 className="display-3 text-info">Education</h4>
               </div>
             </div>
             <Row className="row-grid align-items-center">
@@ -30,18 +30,6 @@ const Education = () => {
               })}
             </Row>
           </Container>
-          <div className="separator separator-bottom separator-skew zindex-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
-            </svg>
-          </div>
         </section>
       </Fade>
     )
