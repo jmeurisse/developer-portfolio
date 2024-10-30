0b4e3e9745f97e75ef07d770e56bc0326477e5e1
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-29 21:40:25 -0700

    Sim Tools

--- containers/Software.tsx
+++ containers/Software.tsx
@@ -17,7 +17,7 @@ const Software = () => {
                 </div>
               </div>
               <div className="pl-4">
-                <h4 className="display-3 text-info">Simulation Software</h4>
+                <h4 className="display-3 text-info">Simulation Tools</h4>
               </div>
             </div>
             <Row className="row-grid align-items-center">
