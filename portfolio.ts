72353cac268875ceaa837c7e0e7ba08a623685ec
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-29 23:11:58 -0700

    tools

--- portfolio.ts
+++ portfolio.ts
@@ -221,7 +221,7 @@ export const software: SoftwareType[] = [
            "My Role: Supervisor since 2019, overseeing code development, research, and long-term planning.",
            "Release: Sandia Open Source",
            "Deployement: HPC modules, user guides, GPU-accelerated computations through Kokkos",
-           "Validation: Comparisons with arc-jet and wind tunnel test data"],
+           "Validation: Comparisons with arc-jet and flight data"],
     github: "https://github.com/sparta/sparta",
     link: "https://ntrs.nasa.gov/api/citations/20220008045/downloads/FAR_paper_PMM.pdf",
     gifSrc: "/img/nasa/sparta.gif",
