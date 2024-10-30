ee5a880f8bb8e781ede2df22d1c38aa830771ebc
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-29 21:33:03 -0700

    coreSkills bg + color + svg separator

--- containers/Proficiency.tsx
+++ containers/Proficiency.tsx
@@ -17,7 +17,7 @@ const CoreSkills = () => {
                 </div>
               </div>
               <div className="pl-4">
-                <h4 className="display-3 text-info">Key Competencies</h4>
+                <h4 className="display-3 text-white">Key Competencies</h4>
               </div>
             </div>
             <Row className="row-grid align-items-center">
@@ -26,6 +26,18 @@ const CoreSkills = () => {
               })}
             </Row>
           </Container>
+          <div className="separator separator-bottom separator-skew zindex-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
+            </svg>
+          </div>
         </section>
       </Fade>
     )
