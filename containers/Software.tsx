e9847546e06a31f69b98516768309ef9e0b962bf
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-30 21:43:00 -0700

    no fade in software

--- containers/Software.tsx
+++ containers/Software.tsx
@@ -7,27 +7,25 @@ import Fade from "react-reveal/Fade";
 const Software = () => {
   return (
     software && (
-      <Fade bottom duration={2000}>
-        <section className="section section-lg">
-          <Container>
-            <div className="d-flex p-4">
-              <div>
-                <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
-                  <i className="ni ni-settings text-info" />
-                </div>
-              </div>
-              <div className="pl-4">
-                <h4 className="display-3 text-info">Simulation Tools</h4>
+      <section className="section section-lg">
+        <Container>
+          <div className="d-flex p-4">
+            <div>
+              <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+                <i className="ni ni-settings text-info" />
               </div>
             </div>
-            <Row className="row-grid align-items-center">
-              {software.map((data, i) => {
-                return <SoftwareCard key={i} {...data} />;
-              })}
-            </Row>
-          </Container>
-        </section>
-      </Fade>
+            <div className="pl-4">
+              <h4 className="display-3 text-info">Simulation Tools</h4>
+            </div>
+          </div>
+          <Row className="row-grid align-items-center">
+            {software.map((data, i) => {
+              return <SoftwareCard key={i} {...data} />;
+            })}
+          </Row>
+        </Container>
+      </section>
     )
   );
 };
