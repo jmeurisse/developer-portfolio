08632e2427872e22f8e817234b690c456b10883c
Jeremie Meurisse <jeremie.meurisse@gmail.com>
2024-10-30 21:53:17 -0700

    no fade for mobile

--- containers/Education.tsx
+++ containers/Education.tsx
@@ -1,39 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EducationCard from "../components/EducationCard";
 import { educationInfo } from "../portfolio";
 import { Container, Row, Col } from "reactstrap";
 import Fade from "react-reveal/Fade";
 
 const Education = () => {
-  return (
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check screen size on component mount
+    const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
+    checkScreenSize();
+
+    // Add event listener to track screen resizing
+    window.addEventListener("resize", checkScreenSize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  const content = (
     educationInfo && (
-      <Fade bottom duration={2000}>
-        <section className="section section-lg">
-          <Container>
-            <div className="d-flex px-3">
-              <div>
-                <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
-                  <i className="ni ni-books text-info" />
-                </div>
-              </div>
-              <div className="pl-4">
-                <h4 className="display-3 text-info">Education</h4>
+      <section className="section section-lg">
+        <Container>
+          <div className="d-flex px-3">
+            <div>
+              <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+                <i className="ni ni-books text-info" />
               </div>
             </div>
-            <Row className="row-grid align-items-center">
-              {educationInfo.map(info => {
-                return (
-                  <Col className="order-lg-1" lg="6" key={info.schoolName}>
-                    <EducationCard {...info} />
-                  </Col>
-                );
-              })}
-            </Row>
-          </Container>
-        </section>
-      </Fade>
+            <div className="pl-4">
+              <h4 className="display-3 text-info">Education</h4>
+            </div>
+          </div>
+          <Row className="row-grid align-items-center">
+            {educationInfo.map(info => {
+              return (
+                <Col className="order-lg-1" lg="6" key={info.schoolName}>
+                  <EducationCard {...info} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </section>
     )
   );
+
+  return isMobile ? content : <Fade bottom duration={1000}>{content}</Fade>;
 };
 
 export default Education;
--- containers/Experience.tsx
+++ containers/Experience.tsx
@@ -1,48 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { experience } from "../portfolio";
 import { Container, Row } from "reactstrap";
 import ExperienceCard from "../components/ExperienceCard";
 import Fade from "react-reveal/Fade";
 
 const Experience = () => {
-  return (
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check screen size on component mount
+    const checkScreenSize = () => setIsMobile(window.innerWidth < 768);
+    checkScreenSize();
+
+    // Add event listener to track screen resizing
+    window.addEventListener("resize", checkScreenSize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  const content = (
     experience && (
-      <Fade bottom duration={2000}>
-        <section className="section pb-0 bg-gradient-info my-5">
-          <Container>
-            <div className="d-flex p-4">
-              <div>
-                <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
-                  <i className="ni ni-briefcase-24 text-info" />
-                </div>
-              </div>
-              <div className="pl-4">
-                <h4 className="display-3 text-white">Experience</h4>
+      <section className="section pb-0 bg-gradient-info my-5">
+        <Container>
+          <div className="d-flex p-4">
+            <div>
+              <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+                <i className="ni ni-briefcase-24 text-info" />
               </div>
             </div>
-            <Row className="row-grid align-items-start">
-              {experience.map((data, i) => {
-                return <ExperienceCard key={i} {...data} />;
-              })}
-            </Row>
-            <div style={{ height: "4rem" }} />
-          </Container>
-          <div className="separator separator-bottom separator-skew zindex-100" style={{ marginBottom: "-3px" }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
-            </svg>
+            <div className="pl-4">
+              <h4 className="display-3 text-white">Experience</h4>
+            </div>
           </div>
-        </section>
-      </Fade>
+          <Row className="row-grid align-items-start">
+            {experience.map((data, i) => {
+              return <ExperienceCard key={i} {...data} />;
+            })}
+          </Row>
+          <div style={{ height: "4rem" }} />
+        </Container>
+        <div className="separator separator-bottom separator-skew zindex-100" style={{ marginBottom: "-3px" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon className="fill-white" points="2560 0 2560 100 0 100" />
+          </svg>
+        </div>
+      </section>
     )
   );
+
+  return isMobile ? content : <Fade bottom duration={1000}>{content}</Fade>;
 };
 
 export default Experience;
